ca4498574bf38513eb033510a6f36d549f0bf5b4
Oleg Butuzov <butuzov@users.noreply.github.com>
2021-12-27 15:02:36 +0200

    Revert "refactor: patterns `OrDone` & `OrContextDone`"

--- cancel.go
+++ cancel.go
@@ -2,11 +2,11 @@ package harmony
 
 import "context"
 
-// OrContextDone will return a new unbuffered channel of type `T`
+// CancelWithContext will return a new channel unbuffered channel of type `T`
 // that serves as a pipeline for the incoming channel. Channel is closed once
-// the context is canceled or the incoming channel is closed. This is variation
-// or the pattern that usually called `OrDone`, `Cancel`.
-func OrContextDone[T any](ctx context.Context, incoming <-chan T) <-chan T {
+// the context is canceled or the incoming channel is closed. This pattern
+// usually called `Done`, `OrDone`, `Cancel`.
+func CancelWithContext[T any](ctx context.Context, incoming <-chan T) <-chan T {
 	ch := make(chan T)
 
 	go func() {
@@ -23,39 +23,6 @@ func OrContextDone[T any](ctx context.Context, incoming <-chan T) <-chan T {
 
 				select {
 				case <-ctx.Done():
-					return
-				case ch <- val:
-				}
-
-			}
-		}
-	}()
-
-	return ch
-}
-
-// OrDone will return a new unbuffered channel of type `T`
-// that serves as a pipeline for the values from the incoming channel. Channel
-// is closed once the context is canceled or the incoming channel is closed.
-// This is the pattern that usually called `OrDone`, `Cancel`.
-func OrDone[T any](done chan struct{}, incoming <-chan T) <-chan T {
-	ch := make(chan T)
-
-	go func() {
-		defer close(ch)
-
-		for {
-			select {
-			case <-done:
-				return
-			case val, ok := <-incoming:
-				if !ok {
-					return
-				}
-
-				select {
-				case <-done:
-					return
 				case ch <- val:
 				}
 
--- cancel_test.go
+++ cancel_test.go
@@ -3,7 +3,6 @@ package harmony_test
 import (
 	"context"
 	"fmt"
-	"math/rand"
 	"reflect"
 	"testing"
 	"time"
@@ -11,95 +10,48 @@ import (
 	"github.com/butuzov/harmony"
 )
 
-func ExampleOrContextDone() {
-	var (
-		done     = make(chan struct{})
-		incoming = make(chan int)
-		outgoing = harmony.OrDone(done, incoming)
-		results  []int
-	)
+func ExampleCancelWithContext() {
+	icnoming := make(chan int)
 
-	// producer
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Creating cancelable pipeline for incoming chan.
+	chOut := harmony.CancelWithContext(ctx, icnoming)
+
 	go func() {
-		defer close(incoming)
-		for i := 1; i < 10000; i++ {
-			incoming <- i
+		defer close(icnoming)
+		for i := 1; i < 100; i++ {
+			icnoming <- i
 		}
 	}()
 
-	// consumer
-	for val := range outgoing {
-		results = append(results, val)
-		// We going to cancel execution once we reach any number devisable by 7
-		if val%7 == 0 {
-			close(done)
+	var res []int
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+
+		for val := range chOut {
+			res = append(res, val)
+
+			// We going to cancel execution once we reach any number devisable by 7
+			if val%7 == 0 {
+				cancel()
+			}
+
+			time.Sleep(time.Millisecond)
+
 		}
-	}
+	}()
 
 	<-done
 
-	fmt.Println(results)
+	fmt.Println(res)
 	// Output: [1 2 3 4 5 6 7]
 }
 
-func TestOrDone(t *testing.T) {
-	var (
-		done     = make(chan struct{})
-		incoming = make(chan int)
-		outgoing = harmony.OrDone(done, incoming)
-		limit    = rand.Intn(25_000) // rand to make it shorter.
-	)
-
-	// producer
-	go func() {
-		defer close(incoming)
-		for i := 1; i <= limit; i++ {
-			incoming <- i
-		}
-	}()
-
-	// consumer
-	var lastRead int
-	for val := range outgoing {
-		lastRead = val
-	}
-
-	if lastRead != limit {
-		t.Errorf("last(%d) vs want(%d)", lastRead, limit)
-	}
-}
-
-func TestOrDone_WithDone(t *testing.T) {
-	var (
-		done     = make(chan struct{})
-		incoming = make(chan int)
-		outgoing = harmony.OrDone(done, incoming)
-		limit    = 25_000
-	)
-
-	// producer
-	go func() {
-		defer close(incoming)
-		for i := 1; i <= limit; i++ {
-			incoming <- i
-		}
-	}()
-
-	// close done after 0.001 sec... (approx ~1k channel messages)
-	timer := time.AfterFunc(time.Millisecond, func() { close(done) })
-	t.Cleanup(func() { timer.Stop() })
-
-	var lastRead int
-	for val := range outgoing {
-		lastRead = val
-	}
-
-	if lastRead == limit { // Last Read is limit, if we not close done.
-		t.Errorf("last(%d) vs want(%d)", lastRead, limit)
-	}
-}
-
-func TestOrContextDone(t *testing.T) {
+func TestCancelWithContext_ContextNotUsed(t *testing.T) {
 	incoming := make(chan int)
 	go func() {
 		defer close(incoming)
@@ -109,7 +61,7 @@ func TestOrContextDone(t *testing.T) {
 	}()
 
 	// Creating cancelable pipeline for incoming chan.
-	chOut := harmony.OrContextDone(context.Background(), incoming)
+	chOut := harmony.CancelWithContext(context.Background(), incoming)
 	var res []int
 	want := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	for val := range chOut {
@@ -118,37 +70,5 @@ func TestOrContextDone(t *testing.T) {
 
 	if !reflect.DeepEqual(res, want) {
 		t.Errorf("want %v vs got %v", want, res)
-	}
-}
-
-func TestOrContextDone_WithContext(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	t.Cleanup(cancel)
-
-	var (
-		incoming = make(chan int)
-		outgoing = harmony.OrContextDone(ctx, incoming)
-		limit    = 25_000
-	)
-
-	// producer
-	go func() {
-		defer close(incoming)
-		for i := 1; i <= limit; i++ {
-			incoming <- i
-		}
-	}()
-
-	// close done after 0.001 sec... (approx ~1k channel messages)
-	timer := time.AfterFunc(time.Millisecond, func() { cancel() })
-	t.Cleanup(func() { timer.Stop() })
-
-	var lastRead int
-	for val := range outgoing {
-		lastRead = val
-	}
-
-	if lastRead == limit { // Last Read is limit, if we not close done.
-		t.Errorf("last(%d) vs want(%d)", lastRead, limit)
 	}
 }
