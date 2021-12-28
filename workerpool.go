2233974f787afdc76a7c4694aa52fdd5b33ccd43
Oleg Butuzov <butuzov@made.ua>
2021-12-28 14:17:20 +0200

    refactor: update `WorkerPool` pattern

--- workerpool.go
+++ workerpool.go
@@ -4,16 +4,51 @@ import (
 	"context"
 )
 
-// WorkerPool returns channel of generic type `T` which excepts jobs of the same type
-// for some number of workers that do workerFn. If you want to stop WorkerPool, close
-// the jobQueue channel.
-func WorkerPool[T any](totalWorkers int, workerFn func(T)) chan<- T {
-	return WorkerPoolWithContext(context.Background(), totalWorkers, workerFn)
+// WorkerPoolWithDone returns channel of generic type `T` which excepts jobs of
+// the same type for some number of workers that do workerFn. If you want to
+// stop WorkerPool, close the jobQueue channel or close a `done` chan.
+func WorkerPoolWithDone[T any](done chan struct{}, totalWorkers int, workerFn func(T)) chan<- T {
+	ch := make(chan T) // channel for the jobs.
+	busyWorkers := make(chan token, totalWorkers)
+
+	go func() {
+		// wait for all workers to finish.
+		defer func() {
+			for i := 0; i < totalWorkers; i++ {
+				busyWorkers <- token{}
+			}
+		}()
+
+		for {
+			var job T
+
+			select {
+			case <-done:
+				return
+			case tmp, ok := <-ch:
+				if !ok {
+					return
+				}
+				job = tmp
+			}
+
+			// Execution is blocked until we able to get one more work permit for this
+			// job. Pattern described by Bryan C. Miles
+			busyWorkers <- token{}
+			go func() {
+				workerFn(job)
+				<-busyWorkers
+			}()
+
+		}
+	}()
+
+	return ch
 }
 
-// WorkerPool returns channel of generic type `T` which excepts jobs of the same type
-// for some number of workers that do workerFn. If you want to stop WorkerPool, close
-// the jobQueue channel or cancel the context.
+// WorkerPoolWithContext returns channel of generic type `T` which excepts jobs
+// of the same type for some number of workers that do workerFn. If you want to
+// stop WorkerPool, close the jobQueue channel or cancel the context.
 func WorkerPoolWithContext[T any](ctx context.Context, totalWorkers int, workerFn func(T)) chan<- T {
 	ch := make(chan T) // channel for the jobs.
 	busyWorkers := make(chan token, totalWorkers)
